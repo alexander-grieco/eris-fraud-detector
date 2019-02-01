1be916600c115718ec808c64ab23c632d354e7ad
Alex Grieco <agrieco992@gmail.com>
2019-02-01 00:12:57 -0500

    EOD changes 1-31

--- src/kafka/pv-producer.py
+++ src/kafka/pv-producer.py
@@ -5,14 +5,15 @@ from confluent_kafka import avro
 from confluent_kafka.avro import AvroProducer
 import uuid
 import numpy
+import time
+import json
 
 
 NUM_USERS = 500
-PV_TOPIC = 'pageview_test3' 
-USER_TOPIC = 'users_test3'
+PV_TOPIC = 'pageview'
 WEBSITE_NAME = 'https://fakenews.com/'
-GROUP = ['politics/', 'sports/', 'international/', 'business/',
-            'tech/', 'arts/', 'health/','science/', 'social/', 'opinion/']
+GROUP = ['politics/'] #, 'sports/', 'international/', 'business/',
+            #'tech/', 'arts/', 'health/','science/', 'social/', 'opinion/']
 
 # Key Schema definition for a pageview
 key_schema_pv_str = """
@@ -74,77 +75,10 @@ class ProducerAvroPV(object):
             }
 
             self.producer.produce(topic = PV_TOPIC, value=value, key=key)
-
-
+            time.sleep(0.5);
+            print("record created:" + json.dumps(value))
         print("\nFlushing records")
         self.producer.flush()
-
-
-# Key Schema definition for a user
-key_schema_user_str = """
-{
-    "namespace": "test2",
-    "name":"key",
-    "type":"record",
-    "fields" : [
-        {"name" : "email", "type" : "string"}
-    ]
-}
-"""
-
-# Value Schema definition for a user
-value_schema_user_str = """
-{
-   "namespace": "test2",
-   "name": "value",
-   "type": "record",
-   "fields" : [
-        {"name" : "username", "type" : "string"},
-        {"name" : "name", "type" : "string"},
-        {"name" : "birthdate", "type" : "string"},
-        {"name" : "sex", "type" : "string"},
-        {"name" : "address", "type" : "string"},
-        {"name" : "email", "type" : "string"}
-   ]
-}
-"""
-
-#
-value_schema_user = avro.loads(value_schema_user_str)
-key_schema_user = avro.loads(key_schema_user_str)
-
-
-class ProducerAvroUser(object):
-
-    def __init__(self, server, schema_registry):
-        self.producer = AvroProducer(
-            {
-                'bootstrap.servers': server,
-                'schema.registry.url': schema_registry
-            },
-            default_key_schema=key_schema_user,
-            default_value_schema=value_schema_user
-        )
-
-    def user_produce(self):
-        for i in range(NUM_USERS):
-            key = {"email" : users[i]['mail']}
-            value = {
-                "username": users[i]['username'],
-                "name" : users[i]['name'],
-                "birthdate" : users[i]['birthdate'].strftime("%Y-%m-%d %H:%M:%S"),
-                "sex" : users[i]['sex'],
-                "address" : users[i]['address'],
-                "email" : users[i]['mail']
-            }
-
-            self.producer.produce(topic = USER_TOPIC, value=value, key=key)
-
-        print("\nFlushing records")
-        self.producer.flush()
-
-
-
 
 
 if __name__ == "__main__":
@@ -152,15 +86,10 @@ if __name__ == "__main__":
     schema_registry = str(sys.argv[2])
     faker = Faker()
 
-    # users = []
-    # for i in range(NUM_USERS):
-    #     faker.seed((i+9092)*32)
-    #     users.append(faker.simple_profile())
-
-    users = [faker.simple_profile() for _ in range(500)]
-
-    u = ProducerAvroUser(server, schema_registry)
-    u.user_produce()
+    users = []
+    for i in range(NUM_USERS):
+        faker.seed((i+9092)*32)
+        users.append(faker.simple_profile())
 
     p = ProducerAvroPV(server, schema_registry)
     p.pv_produce()
